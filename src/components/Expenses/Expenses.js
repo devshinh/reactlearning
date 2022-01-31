41ffccf5b14c2ca1fb282f9a7b7e62567d3cbb75
System Administrator <root@Atuls-MacBook-Pro.local>
2022-01-30 22:59:24 -0800

    adding react project code till Sec 5, lecture 68

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import ExpenseList from './ExpenseList';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpenseFilter';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+        <Card className='expenses'>
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+          <ExpenseList items={filteredExpenses} />
+
+        </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditHandler = () => {
+    setIsEditing(false);
+  };
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className='new-expense'>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add a New Expense *</button>
+      )}
+      {isEditing && (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditHandler} />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
